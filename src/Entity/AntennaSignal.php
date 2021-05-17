0cb565ba01a66d85c8118150f153104f48d1f17b
cristobal <cristobal@wifi01.provicar.com>
2021-05-17 17:04:23 +0200

    a

--- src/Entity/AntennaSignal.php
+++ src/Entity/AntennaSignal.php
@@ -84,7 +84,7 @@ class AntennaSignal
         return $this->image;
     }
 
-    public function setImage(string $image): self
+    public function setImage(?string $image): self
     {
         $this->image = $image;
 
